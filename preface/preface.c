e28f95a195d46b99baea1c0e2f3622cbf7208672
Yinlei Meng <mengyl4772@gmail.com>
2023-09-16 16:58:00 +0800

    preface

---
+++ preface/preface.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+// 作者推荐的 C 语言相关书籍
+// K & R
+// C: A Reference Manual
+// Expert C Programming
+// 
+// 一些 Linux 专有的编程特性
+// epoll, inotify, capabilities,
+// extended attributes, i-node flags,
+// clone() syscall, /proc file system
+// 
+// 内容组织
+// 1. 背景和概念 => Chapter 1 2 3
+// 2. 系统编程接口的基本特性
+//    文件 I/O => Chapter 4 5
+//    进程 => Chapter 6
+//    内存分配 => Chapter 7
+//    用户和组 => Chapter 8
+//    进程凭证(process credentials) => Chapter 9
+//    时间 => Chapter 10
+//    系统限制和选项 => Chapter 11
+//    获取系统和进程信息 => Chapter 12
+// 3. 系统编程接口的高级特性
+//    文件 I/O 缓冲 => Chapter 13
+//    文件系统 => Chapter 14
+//    文件属性 => Chapter 15
+//    扩展属性 => Chapter 16
+//    访问控制列表 => Chapter 17
+//    目录和链接 => Chapter 18
+//    监控文件事件 => Chapter 19
+//    信号 => Chapter 20 21 22
+//    定时器 => Chapter 23
+// 4. 进程和线程的基本主题
+//    进程的创建、终止、监控子进程和执行程序 => Chapter 24 25 26 27 28
+//    POSIX 线程 => Chapter 29 30 31 32 33
+// 5. 进程和线程的高级主题
+//    进程组、会话(session)和任务控制 => Chapter 34
+//    进程优先级和调度 => Chapter 35
+//    进程资源 => Chapter 36
+//    守护进程(daemons) => Chapter 37
+//    编写安全的特权程序 => Chapter 38
+//    能力(capabilities) => Chapter 39
+//    登录记账(login accounting) => Chapter 40
+//    共享库 => Chapter 41 42
+// 6. 进程间通信
+//    IPC 概览 => Chapter 43
+//    管道和 FIFO => Chapter 44
+//    System V IPC - 消息队列、信号量和共享内存 => Chapter 45 46 47 48
+//    内存映射 => Chapter 49
+//    虚拟内存操作 => Chapter 50
+//    POSIX IPC - 消息队列、信号量和共享内存 => Chapter 51 52 53 54
+//    文件锁定 => Chapter 55
+// 7. 套接字和网络编程
+//    使用套接字的 IPC 和网络编程 => Chapter 56 57 58 59 60 61
+// 8. 高级 I/O 主题
+//    终端 => Chapter 62
+//    其他 I/O 模型 => Chapter 63
+//    伪终端 => Chapter 64
+int main() {
+    printf("Preface\n");
+}
