121971e1cb6da253e971a76b1a7f5895ba66e519
Yinlei Meng <mengyl4772@gmail.com>
2023-09-16 12:10:07 +0800

    ubuntu in vmware

---
+++ vmware/ubuntu.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// 在 VMware 上安装 Ubuntu 虚拟机的步骤
+// 1. 创建新的虚拟机
+// 2. 经典（推荐）
+// 3. 稍后安装操作系统
+// 4. 选择 Linux - Ubuntu 64 位
+// 5. 名字 + 存储位置
+// 6. 将虚拟磁盘存储为单个文件
+// 7. 自定义硬件
+//    1. 内存 >= 8G
+//    2. 处理器内核总数 >= 2
+//    3. 网络适配器 NAT 模式
+//    4. 使用 ISO 映像文件
+// 8. 开启虚拟机
+// 9. Install Ubuntu
+// a. Normal Installation
+// b. Erase disk and install Ubuntu
+// c. 地区选择上海
+// d. 设置用户名和密码
+int main() {
+    printf("Ubuntu in VMware");
+    return 0;
+}
